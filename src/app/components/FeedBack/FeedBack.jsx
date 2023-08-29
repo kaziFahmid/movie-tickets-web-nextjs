7423f1b82fe63fb5cb135644588823387183e083
unknown <nirjhorislam42@gmail.com>
2023-08-29 19:29:30 +0600

    text added

--- src/app/components/FeedBack/FeedBack.jsx
+++ src/app/components/FeedBack/FeedBack.jsx
@@ -55,7 +55,7 @@ const FeedBack = () => {
 </div>
 
 <p className="my-4 mb-0 text-base font-normal leading-relaxed tracking-wide text-gray-400">
-    I've been using XYZ
+    I ve been using XYZ
     for over a year now and their customer service is excellent! Whenever I have a question, the team is always
     available and willing to help. Highly recommend!
 </p>
@@ -123,7 +123,7 @@ const FeedBack = () => {
 </div>
 
 <p className="my-4 mb-0 text-base font-normal leading-relaxed tracking-wide text-gray-400">
-    I've been using XYZ
+    Ive been using XYZ
     for over a year now and their customer service is excellent! Whenever I have a question, the team is always
     available and willing to help. Highly recommend!
 </p>
@@ -194,142 +194,7 @@ const FeedBack = () => {
 </div>
 
 <p className="my-4 mb-0 text-base font-normal leading-relaxed tracking-wide text-gray-400">
-    I've been using XYZ
-    for over a year now and their customer service is excellent! Whenever I have a question, the team is always
-    available and willing to help. Highly recommend!
-</p>
-
-
-<div className="mt-6 flex items-center gap-6 ">
-    <div className="h-10 w-10 overflow-hidden rounded-full shadow-sm outline-neutral-800">
-        <div className="relative inline-block overflow-hidden rounded-lg border-neutral-800">
-            <Image alt="people" src="https://randomuser.me/api/portraits/women/2.jpg" width={50} height={50}
-                decoding="async" data-nimg="1" className="inline-block " loading="lazy" />
-        </div>
-    </div>
-    <div>
-        <p className="leading-relaxed tracking-wide text-gray-200">Melissa Smith</p>
-        <p className="text-xs leading-relaxed tracking-wide text-gray-400">Marketing Manager</p>
-    </div>
-</div>
-
-
-</div>
-
-
-
-
-
-
-<div className="flex flex-col justify-between rounded-md border border-neutral-800 bg-black p-8 shadow-sm max-w-sm mx-auto mt-24">
-
-
-<div className="text-yellow-500 flex gap-2">
-    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
-        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path
-            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-        </path>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
-        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path
-            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-        </path>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
-        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path
-            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-        </path>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
-        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path
-            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-        </path>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
-        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path
-            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-        </path>
-    </svg>
-</div>
-
-<p className="my-4 mb-0 text-base font-normal leading-relaxed tracking-wide text-gray-400">
-    I've been using XYZ
-    for over a year now and their customer service is excellent! Whenever I have a question, the team is always
-    available and willing to help. Highly recommend!
-</p>
-
-
-<div className="mt-6 flex items-center gap-6 ">
-    <div className="h-10 w-10 overflow-hidden rounded-full shadow-sm outline-neutral-800">
-        <div className="relative inline-block overflow-hidden rounded-lg border-neutral-800">
-            <Image alt="people" src="https://randomuser.me/api/portraits/women/2.jpg" width={50} height={50}
-                decoding="async" data-nimg="1" className="inline-block " loading="lazy" />
-        </div>
-    </div>
-    <div>
-        <p className="leading-relaxed tracking-wide text-gray-200">Melissa Smith</p>
-        <p className="text-xs leading-relaxed tracking-wide text-gray-400">Marketing Manager</p>
-    </div>
-</div>
-
-
-</div>
-
-
-
-<div className="flex flex-col justify-between rounded-md border border-neutral-800 bg-black p-8 shadow-sm max-w-sm mx-auto mt-24">
-
-
-<div className="text-yellow-500 flex gap-2">
-    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
-        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path
-            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-        </path>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
-        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path
-            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-        </path>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
-        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path
-            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-        </path>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
-        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path
-            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-        </path>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
-        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path
-            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-        </path>
-    </svg>
-</div>
-
-<p className="my-4 mb-0 text-base font-normal leading-relaxed tracking-wide text-gray-400">
-    I've been using XYZ
+    Ive been using XYZ
     for over a year now and their customer service is excellent! Whenever I have a question, the team is always
     available and willing to help. Highly recommend!
 </p>
@@ -398,7 +263,142 @@ const FeedBack = () => {
 </div>
 
 <p className="my-4 mb-0 text-base font-normal leading-relaxed tracking-wide text-gray-400">
-    I've been using XYZ
+    Ive been using XYZ
+    for over a year now and their customer service is excellent! Whenever I have a question, the team is always
+    available and willing to help. Highly recommend!
+</p>
+
+
+<div className="mt-6 flex items-center gap-6 ">
+    <div className="h-10 w-10 overflow-hidden rounded-full shadow-sm outline-neutral-800">
+        <div className="relative inline-block overflow-hidden rounded-lg border-neutral-800">
+            <Image alt="people" src="https://randomuser.me/api/portraits/women/2.jpg" width={50} height={50}
+                decoding="async" data-nimg="1" className="inline-block " loading="lazy" />
+        </div>
+    </div>
+    <div>
+        <p className="leading-relaxed tracking-wide text-gray-200">Melissa Smith</p>
+        <p className="text-xs leading-relaxed tracking-wide text-gray-400">Marketing Manager</p>
+    </div>
+</div>
+
+
+</div>
+
+
+
+<div className="flex flex-col justify-between rounded-md border border-neutral-800 bg-black p-8 shadow-sm max-w-sm mx-auto mt-24">
+
+
+<div className="text-yellow-500 flex gap-2">
+    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
+        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+        </path>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
+        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+        </path>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
+        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+        </path>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
+        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+        </path>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
+        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+        </path>
+    </svg>
+</div>
+
+<p className="my-4 mb-0 text-base font-normal leading-relaxed tracking-wide text-gray-400">
+    Ive been using XYZ
+    for over a year now and their customer service is excellent! Whenever I have a question, the team is always
+    available and willing to help. Highly recommend!
+</p>
+
+
+<div className="mt-6 flex items-center gap-6 ">
+    <div className="h-10 w-10 overflow-hidden rounded-full shadow-sm outline-neutral-800">
+        <div className="relative inline-block overflow-hidden rounded-lg border-neutral-800">
+            <Image alt="people" src="https://randomuser.me/api/portraits/women/2.jpg" width={50} height={50}
+                decoding="async" data-nimg="1" className="inline-block " loading="lazy" />
+        </div>
+    </div>
+    <div>
+        <p className="leading-relaxed tracking-wide text-gray-200">Melissa Smith</p>
+        <p className="text-xs leading-relaxed tracking-wide text-gray-400">Marketing Manager</p>
+    </div>
+</div>
+
+
+</div>
+
+
+
+
+
+
+<div className="flex flex-col justify-between rounded-md border border-neutral-800 bg-black p-8 shadow-sm max-w-sm mx-auto mt-24">
+
+
+<div className="text-yellow-500 flex gap-2">
+    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
+        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+        </path>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
+        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+        </path>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
+        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+        </path>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
+        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+        </path>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-500" width="20" height="20" viewBox="0 0 24 24"
+        strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+        </path>
+    </svg>
+</div>
+
+<p className="my-4 mb-0 text-base font-normal leading-relaxed tracking-wide text-gray-400">
+    Ive been using XYZ
     for over a year now and their customer service is excellent! Whenever I have a question, the team is always
     available and willing to help. Highly recommend!
 </p>
